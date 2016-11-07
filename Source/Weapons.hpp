847bcce6419f10e0b6bf8b8589bcd69ad22aa3b9
Mike <m.hilhorst@hotmail.nl>
2016-11-07 19:57:59 +0100

    Added Enum class "Weapons", implemented enums in keypads

---
+++ Source/Weapons.hpp
@@ -0,0 +1,24 @@
+//
+// Created by MuscioCraft on 7-11-2016.
+//
+
+#ifndef SKILLED_EAGLE_WEAPONS_HPP
+#define SKILLED_EAGLE_WEAPONS_HPP
+
+
+enum class Weapons {
+    AK47 = 1,
+    M9 = 2,
+    MP5 = 3,
+    UZI = 4,
+    RPG = 5,
+    IPOD = 6,
+    TIMMY = 7,
+    PARKER = 8,
+    JIMMY = 9
+
+};
+
+
+
+#endif //SKILLED_EAGLE_WEAPONS_HPP
