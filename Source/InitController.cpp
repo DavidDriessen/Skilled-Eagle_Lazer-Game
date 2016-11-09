ee9956d1af7e47bc09a86d0415320448c4feb952
Mike <m.hilhorst@hotmail.nl>
2016-11-09 13:48:53 +0100

    theoretical InitController works with IrSender::encode_stream();

--- Source/InitController.cpp
+++ Source/InitController.cpp
@@ -5,14 +5,14 @@
 #include "InitController.hpp"
 
 void InitController::PlayerGetter(int player){
-    data[0] = player;
+    data[0] = (char)player;
 }
 
 void InitController::WeaponGetter(int weapon){
-    data[1] = weapon;
+    data[1] = (char)weapon;
 }
 
 void InitController::CommandGetter(int command){
-    data[2] = command;
-    hwlib::cout << data[0] << data[1] << data[2];
+    data[2] = (char)command;
+    IRsender::encode_stream(data[0], data[1], data[2]);
 }
