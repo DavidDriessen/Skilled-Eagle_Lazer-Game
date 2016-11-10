c40de40e0bd2402f8d7abd57ce8f001dc0e2514c
MSI\david <daviddriessen@live.nl>
2016-11-10 12:08:55 +0100

    irsend

--- Source/InitController.cpp
+++ Source/InitController.cpp
@@ -5,21 +5,24 @@
 #include "InitController.hpp"
 
 //! Gets and sends Player ID
-void InitController::PlayerGetter(int player){
-    data[0] = (char)player;
-    send.write_speler(data[0]);
+void InitController::PlayerGetter(int player) {
+    data[0] = (char) player;
 }
 
 //! Gets and sends Weapon ID
-void InitController::WeaponGetter(int weapon){
-    data[1] = (char)weapon;
-    send.write_data(data[1]);
+void InitController::WeaponGetter(int weapon) {
+    data[1] = (char) weapon;
 }
 
 //! Gets and sends Command ID
-void InitController::CommandGetter(int command){
-    data[2] = (char)command;
-    send.write_command(data[2]);
+void InitController::CommandGetter(int command) {
+    if (command == 1) {
+        send.send_player_and_weapon((unsigned char) data[0], (unsigned char) data[1]);
+    } else if (command == 2) {
+        send.send_start_game();
+    } else {
+        send.send_gametime(command);
+    }
 
 
 }
